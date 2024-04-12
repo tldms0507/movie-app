40fc373e95295630fd2aa367080579dd180af5ae
tldms0507 <sieun050783@gmail.com>
2024-04-12 19:22:29 +0900

    제발 나왕라에

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 // import { useEffect, useState } from "react";
 // import Movie from "../components/Movie";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
-import "./App.css"
+import "./App.css";
 
 function App() {
   return (
-
-      <Routes>
-        <Route path="/movie/:id" element={<Detail />}></Route>
-        {/* 페이지 링크 뒤에 "/movie"를 붙이면 Detail페이지가 나오게 됨 */}
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-
+    <h1>TLQKF</h1>
+    // <Routes>
+    //   <Route path="/movie/:id" element={<Detail />}></Route>
+    //   {/* 페이지 링크 뒤에 "/movie"를 붙이면 Detail페이지가 나오게 됨 */}
+    //   <Route path="/" element={<Home />}></Route>
+    // </Routes>
   );
 }
 //<Route path="/"> : Home Route 렌더링("/"url에 있을 때 Home 컴포넌트 렌더링)
