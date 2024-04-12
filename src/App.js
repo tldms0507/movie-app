3f1125a43505d36d6b5695cb0bf0c8e28b4e7627
tldms0507 <sieun050783@gmail.com>
2024-04-12 18:19:06 +0900

    라우터 해결!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import { useEffect, useState } from "react";
 // import Movie from "../components/Movie";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
@@ -9,13 +9,13 @@ import "./App.css"
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+
       <Routes>
         <Route path="/movie/:id" element={<Detail />}></Route>
         {/* 페이지 링크 뒤에 "/movie"를 붙이면 Detail페이지가 나오게 됨 */}
         <Route path="/" element={<Home />}></Route>
       </Routes>
-    </Router>
+
   );
 }
 //<Route path="/"> : Home Route 렌더링("/"url에 있을 때 Home 컴포넌트 렌더링)
